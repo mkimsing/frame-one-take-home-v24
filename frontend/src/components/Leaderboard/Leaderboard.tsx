426707715dce2ef94226b87b2450383b0720ef7e
Michael Kim Sing <mkimsing95@gmail.com>
2024-01-10 15:36:43 -0800

    Update badge and add loader

--- frontend/src/components/Leaderboard/Leaderboard.tsx
+++ frontend/src/components/Leaderboard/Leaderboard.tsx
@@ -1,14 +1,15 @@
 import LeaderboardItem from "./LeaderboardItem";
 import { useLeaderboard } from "./useLeaderboard";
-
+import { FiLoader } from "react-icons/fi";
 export default function Leaderboard() {
-  const { leaderboardData } = useLeaderboard();
+  const { leaderboardData, leaderboardLoading } = useLeaderboard();
 
   return (
     <div className="flex flex-col border-2 p-4 rounded-xl mt-5 max-w-screen-md mx-auto">
       <p className="text-2xl font-bold mb-4">
         Community Experience Point Leaders
       </p>
+      {Boolean(leaderboardLoading) && <FiLoader size={45} className="m-auto" />}
       {leaderboardData &&
         leaderboardData.map((entry, index) => {
           return (
--- frontend/src/components/Leaderboard/LeaderboardItem.tsx
+++ frontend/src/components/Leaderboard/LeaderboardItem.tsx
@@ -34,17 +34,17 @@ export default function LeaderboardItem({ entry, rank }: Props) {
   const bgColors: { [key: number]: string } = {
     1: "#b2d8d8",
     2: "#d8b2d8",
-    3: "#f4a460",
+    3: "#f1d585",
   };
   return (
     <div
-      className="flex items-center bg-red-200 p-4 rounded-lg"
+      className="flex flex-col-reverse items-center sm:flex-row sm:items-center bg-red-200 p-4 rounded-lg"
       style={{
-        backgroundColor: bgColors[rank] || "#efeff2",
+        backgroundColor: bgColors[rank] || "#efefef",
       }}
     >
       <img src={logo} alt="Community logo" className="rounded-full w-20 h-20" />
-      <div className="ml-5">
+      <div className="mb-1 sm:mb-0 sm:ml-5">
         <div className="text-lg font-bold">{name}</div>
         <div className="text-gray-600 text-md">{`${totalUsers} members`}</div>
       </div>
@@ -52,11 +52,13 @@ export default function LeaderboardItem({ entry, rank }: Props) {
         <div>{`${totalExperience} pts`}</div>
       </div>
       <div className="w-16 flex items-center justify-center">
-        {rank <= 3 ? (
-          createTrophy(rank)
-        ) : (
-          <div className="text-xl font-bold">{`${rank}`}</div>
-        )}
+        <div className="p-4 bg-gray-50 rounded-full w-16 h-16 flex justify-center items-center mb-3 sm:mb-0 ">
+          {rank <= 3 ? (
+            createTrophy(rank)
+          ) : (
+            <div className="text-xl font-bold">{`${rank}`}</div>
+          )}
+        </div>
       </div>
     </div>
   );
